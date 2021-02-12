f8f0be805551764dbd29eff39fabc0db582c59bf
AbdulRahimFarooqui <67377097+AbdulRahimFarooqui@users.noreply.github.com>
2021-02-12 17:20:58 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getQuestions } from './services/service_questions'
+import { UsefulQuestion } from './Types/quiz_types'
+import QuestionCard from './Compnents/QuestionDisplay';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+
+  let [quiz, setquiz] = useState<UsefulQuestion[]>([])
+  let [showResult, setShowResult] = useState(false)
+  let [currentStep, setCurrentStep] = useState(0)
+  let [score, setScore] = useState(0)
+
+  useEffect(() => {
+    async function fetchData() {
+      const questions: UsefulQuestion[] = await getQuestions(5, "easy")
+      console.log(questions);
+      setquiz(questions);
+    }
+    fetchData();
+  }, [])
+
+  const handleSubmit = (e: React.FormEvent<EventTarget>, userAns: string) => {
+    if (userAns === quiz[currentStep].answer) {
+      setScore(++score)
+    }
+    e.preventDefault();
+    if (currentStep !== quiz.length - 1) {
+      setCurrentStep(++currentStep);
+    }
+    else {
+      setShowResult(true);
+    }
+  }
+
+  if (!quiz.length) {
+    return (<h3>Loading...</h3>)
+  }
+  if (showResult) {
+    return (
+      <div className="question-container result-container">
+        <h2>Result</h2>
+        <p className="result-text">
+          Your Final Result is
+          <b> {score}</b> out of <b>{quiz.length}</b>
+          <p>PLEASE REFRESH THIS PAGE TO PLAY AGAIN!</p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <div className="Intro-Text">
+        <p className="Welcome">
+          <b>
+            Welcome to The Trivia-5-Questions Quiz App
+          </b>
+        </p>
+        <p>
+          Here We Will Test Your General-Knowledge By Asking Five Questions In A Row.
+        </p>
+        <p>
+          For Each Answer Please Select A <b>Correct Answer</b> From The Given Options!
+        </p>
+      </div>
+      
+
+      <QuestionCard
+        options={quiz[currentStep].options}
+        question={quiz[currentStep].question}
+        callback={handleSubmit}
+        questionNo={currentStep}
+        totalQuestions={quiz.length}
+      />
     </div>
   );
 }
